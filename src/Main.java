de899d29fdef7df798e356e037ed0289aa3f8638
Daniel <Daniel.kotrba@gmx.at>
2020-02-24 08:59:31 +0100

    Berechnung des maximums commit v.1

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Enter a few numbers!  Enter q to quit:");
+        Scanner scan = new Scanner(System.in);
+
+        int[] number = new int[0];
+
+        while (!scan.hasNext("q") && !scan.hasNext("Q")) {
+            int value = scan.nextInt();
+            int[] array = new int[number.length + 1];
+            for (int i = 0; i < number.length;i++) {
+                array[i] = number[i];
+
+            }
+            array[number.length] = value;
+            number = array;
+            System.out.println(value);
+        }
+        System.out.println(number.length);
+
+        int biggestNumber = 0;
+        for (int i = 0;i < number.length; i++) {
+            if (biggestNumber < number[i]) {
+                biggestNumber = number[i];
+            }
+        }
+
+        System.out.println(biggestNumber);
+
+    }
+}
